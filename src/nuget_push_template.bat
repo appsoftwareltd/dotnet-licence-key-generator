31f14cc6724be8a9717d16b99d75a52c0ab02c7b
Gareth Brown <gareth.brown@appsoftware.com>
2020-08-31 21:08:05 +0100

    Commenting

--- src/nuget_push_template.bat
+++ src/nuget_push_template.bat
@@ -6,6 +6,8 @@ REM Replace version and API key in nuget_push.bat (VCS ignored)
 dotnet build AppSoftware.LicenceEngine.KeyGenerator/AppSoftware.LicenceEngine.KeyGenerator.csproj --configuration Release
 dotnet build AppSoftware.LicenceEngine.KeyVerification/AppSoftware.LicenceEngine.KeyVerification.csproj --configuration Release
 
+REM TODO: Replace with equivalent dotnet nuget commands
+
 nuget pack AppSoftware.LicenceEngine.KeyGenerator/AppSoftware.LicenceEngine.KeyGenerator.csproj -OutputDirectory packages -Properties Configuration=Release
 nuget pack AppSoftware.LicenceEngine.KeyVerification/AppSoftware.LicenceEngine.KeyVerification.csproj -OutputDirectory packages -Properties Configuration=Release
 
