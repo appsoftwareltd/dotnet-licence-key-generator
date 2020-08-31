d2f75422bea3d726d494ac7a5a2b2f11ac97f192
Gareth Brown <gareth.brown@appsoftware.com>
2020-08-31 21:07:16 +0100

    Nuget push configuration and script

---
+++ src/nuget_push_template.bat
@@ -0,0 +1,15 @@
+REM https://www.nuget.org/packages/AppSoftware.LicenceEngine.KeyGenerator/
+REM https://www.nuget.org/packages/AppSoftware.LicenceEngine.KeyVerification/
+
+REM Replace version and API key in nuget_push.bat (VCS ignored)
+
+dotnet build AppSoftware.LicenceEngine.KeyGenerator/AppSoftware.LicenceEngine.KeyGenerator.csproj --configuration Release
+dotnet build AppSoftware.LicenceEngine.KeyVerification/AppSoftware.LicenceEngine.KeyVerification.csproj --configuration Release
+
+nuget pack AppSoftware.LicenceEngine.KeyGenerator/AppSoftware.LicenceEngine.KeyGenerator.csproj -OutputDirectory packages -Properties Configuration=Release
+nuget pack AppSoftware.LicenceEngine.KeyVerification/AppSoftware.LicenceEngine.KeyVerification.csproj -OutputDirectory packages -Properties Configuration=Release
+
+nuget push packages/AppSoftware.LicenceEngine.KeyGenerator.9.9.9.nupkg -Source https://api.nuget.org/v3/index.json -SkipDuplicate -ApiKey xxxxxxxxxxxxxxxxxxxxx
+nuget push packages/AppSoftware.LicenceEngine.KeyVerification.9.9.9.nupkg -Source https://api.nuget.org/v3/index.json -SkipDuplicate -ApiKey xxxxxxxxxxxxxxxxxxxxx
+
+PAUSE
